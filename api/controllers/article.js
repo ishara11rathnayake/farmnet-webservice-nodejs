4ef4d52b7a31902b1f14ca8424645a08556506a9
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-09-29 03:25:32 +0530

    create article route

--- api/controllers/article.js
+++ api/controllers/article.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Article = require("../models/article");
 
 exports.get_thumbnail_url = (req, res, next) => {
+  console.log("============");
   res.status(200).json({
     url: req.file.path
   });
