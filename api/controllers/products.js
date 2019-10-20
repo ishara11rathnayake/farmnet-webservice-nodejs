1e76c764d0f9e36f128ee214efe8badad7e4b8c7
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-10-20 15:44:16 +0530

    changes in product route

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -72,6 +72,7 @@ exports.products_get_all = (req, res, next) => {
 };
 
 exports.products_create_product = (req, res, next) => {
+  console.log(req.body.userId);
   User.findById(req.body.userId)
     .then(user => {
       if (!user) {
